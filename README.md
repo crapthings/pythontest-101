9f28650f2f2b677578289d44249bb1247368d2b9
crapthings <crapthings@gmail.com>
2018-05-23 17:11:47 +0800

    minor update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pythontest-101
+
+### cypher cheatsheet
+https://gist.github.com/DaniSancas/1d5265fc159a95ff457b940fc5046887
--- process_mingxing.py
+++ process_mingxing.py
@@ -2,6 +2,7 @@
 
 import json
 import re
+from pyhanlp import *
 
 mingxingfile = json.loads(open('mingxing_unprocessed.json').read())
 
@@ -20,6 +21,15 @@ for item in mingxingfile:
 
   mingxing['relations'] = re.sub('^\\n', '', item['relations'])
   mingxing['relations'] = re.sub('$\\n', '', item['relations'])
+
+  labels = ''
+  for label in HanLP.segment(mingxing['summary']):
+    word = str(label.word)
+    # print(label.nature, label.word)
+    if str(label.nature) in ['nnt', 'nnd'] and word not in labels:
+      labels += word + '、'
+
+  mingxing['labels'] = labels
 
   mingxinglist.append(mingxing)
 
