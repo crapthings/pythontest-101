79cd5e8830d4f33fac2afc38dc6e669a81de5b20
crapthings <crapthings@gmail.com>
2018-05-23 15:47:48 +0800

    minor update

--- guanxilist.py
+++ guanxilist.py
@@ -5,9 +5,11 @@ from pyhanlp import *
 import pydash
 
 mingxinglist = json.loads(open('mingxing_processed.json', 'r').read())
+guanxifile = open('guanxi1.txt', 'w')
 
 guanxi = {}
 guanxilist = []
+guanxistr = ''
 
 for mingxing in mingxinglist:
   relations = ''.join(mingxing['relations'].split('\n'))
@@ -18,7 +20,6 @@ for mingxing in mingxinglist:
   for ci in fenci:
     word = str(ci.word)
     nature = str(ci.nature)
-    dir(guanxi)
     if len(word) == 1:
       continue
     if word in guanxi.keys():
@@ -28,11 +29,17 @@ for mingxing in mingxinglist:
       continue
 
 for gx in guanxi:
+  nature = HanLP.segment(gx)
   guanxilist.append({
     'name': gx,
-    'count': guanxi[gx]
+    'count': guanxi[gx],
+    'nature': str(nature[0].nature)
   })
 
 guanxilist = pydash.collections.order_by(guanxilist, ['count'], reverse=True)
 
-print(guanxilist)
+for gx in guanxilist:
+  guanxistr = guanxistr + gx.get('name') + ' ' + str(gx.get('count')) + ' ' + str(gx.get('nature')) + '\n'
+
+guanxifile.write(guanxistr)
+guanxifile.close()
